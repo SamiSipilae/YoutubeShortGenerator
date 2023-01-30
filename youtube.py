d10e280fae06ff73594980ccdc1b16ca1028c5eb
Sami Sipilä <eternality@gmail.com>
2023-01-31 00:21:24 +0200

    youtube fixes

--- youtube.py
+++ youtube.py
@@ -21,6 +21,10 @@ def upload(file, description, tags, title):
 
     browser = webdriver.Firefox(options=options)
     browser.get("https://www.youtube.com/")
+    try:
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@aria-label,"Accept the use of cookies and other data for the purposes described")]'))).click()
+    except:
+        pass
     # retrieve cookies from a json file
     for cookie in json.loads(Path('cookies.json').read_text()):
         browser.add_cookie(cookie)
@@ -55,13 +59,12 @@ def upload(file, description, tags, title):
         WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//div[.="Public"]'))).click()
         WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//div[.="Publish"]'))).click()
         WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//ytcp-button[@id="close-button"]'))).click()
+        Path('cookies.json').write_text(
+        json.dumps(browser.get_cookies(), indent=2))
         browser.close()
     except:
         logging.exception("Exception while uploading file:")
         breakpoint()
-
-
-
 
 
 
